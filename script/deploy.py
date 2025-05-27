16b70042636a91295f23d552f3ad585d8df28502
benny <benoitb@tutanota.de>
2025-05-27 20:10:33 +1000

    fix deploy script

--- script/deploy.py
+++ script/deploy.py
@@ -3,8 +3,8 @@ from moccasin.boa_tools import VyperContract
 
 def deploy() -> VyperContract:
     return GlobalFeeSplitter.deploy(
-        "0xa2Bcd1a4Efbd04B63cd03f5aFf2561106ebCCE00", # fee collector
         "0xD16d5eC345Dd86Fb63C6a9C43c517210F1027914", # fee distributor
+        "0xa2Bcd1a4Efbd04B63cd03f5aFf2561106ebCCE00", # fee collector
         "0x40907540d8a6C65c637785e8f8B742ae6b0b9968" # dao proxy
     )
 
