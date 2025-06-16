1069b5ac230ea4845d0cc86d0cf1f1a31ff72841
benny <benoitb@tutanota.de>
2025-06-16 21:31:11 +1000

    Test vest creation

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -1,10 +1,28 @@
 import boa
+from boa.contracts.abi.abi_contract import ABIContractFactory
 
 from script.deploy import deploy
 from tests.conftest import WEEK
 
 CONVEX = "0x989AEb4d175e16225E39E87d0D97A3360524AD80"
 STD = "0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6"
+MIN_VESTING_DURATION = 86400 * 365
+VESTING_ESCROW_CLAIM_ABI = [
+    {
+        "name": "claim",
+        "outputs": [],
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+    {
+        "name": "claim",
+        "outputs": [],
+        "inputs": [{"type": "address", "name": "addr"}],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    },
+]
 
 
 def test_deployment(
@@ -16,7 +34,9 @@ def test_deployment(
     actual_hooker,
     actual_crvusd,
     crvusd_minter,
+    agent,
 ):
+    # Test the deployment & vote creation
     fee_allocator, proposal_id, metadata, logs = deploy()
     assert voting.votesLength() == proposal_id + 1
     prop_details = voting.getVote(proposal_id)
@@ -35,6 +55,7 @@ def test_deployment(
     assert prop_details[1]  # executed
     assert fee_allocator.receiver_weights(community_fund.address) == 1_000
 
+    # Test the distribution workflow
     pre_distribution_collector_balance = actual_crvusd.balanceOf(
         actual_fee_collector.address
     )
@@ -92,3 +113,24 @@ def test_deployment(
         )
 
     assert abs(table[2][4] - 0.10) < 0.01  # 10% goes to community fund ±1%
+
+    # Test that we can create a vest from the community fund using crvusd
+    grantee = boa.env.generate_address()
+    with boa.env.prank(agent.address):
+        vesting_contract_address = community_fund.deploy_vesting_contract(
+            actual_crvusd.address,
+            grantee,
+            post_distribution_community_balance,
+            False,
+            MIN_VESTING_DURATION,
+        )
+
+    boa.env.time_travel(seconds=MIN_VESTING_DURATION + 1)
+
+    vesting_contract = ABIContractFactory(
+        "Vesting", VESTING_ESCROW_CLAIM_ABI
+    ).at(vesting_contract_address)
+    vesting_contract.claim(grantee)
+    assert (
+        actual_crvusd.balanceOf(grantee) == post_distribution_community_balance
+    )
